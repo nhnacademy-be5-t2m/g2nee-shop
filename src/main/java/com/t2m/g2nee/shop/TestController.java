862ba106ff4ed541ea49c1ba2b0010f3aa62ba4c
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-03-14 15:08:30 +0900

    create testController

---
+++ src/main/java/com/t2m/g2nee/shop/TestController.java
@@ -0,0 +1,14 @@
+package com.t2m.g2nee.shop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String hello(){
+
+        return "hello";
+    }
+}
