ff372fc54aeb207255d6aa64edf5f99373569cc1
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-03-22 14:43:19 +0900

    :page_facing_up: modify testController
    /test 로 매핑되던것을 /hello로 변경

--- src/main/java/com/t2m/g2nee/shop/TestController.java
+++ src/main/java/com/t2m/g2nee/shop/TestController.java
@@ -1,14 +1,19 @@
 package com.t2m.g2nee.shop;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class TestController {
 
-    @GetMapping("/hello")
-    public String hello(){
+    @Value("${server.port}")
+    private String port;
 
-        return "hello g2nee-shop";
+    @GetMapping
+    public String getHello(){
+        return port + ":hello g2nee shop";
     }
 }
