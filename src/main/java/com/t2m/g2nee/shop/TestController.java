b8b1dec938448c587d031ad52997a35e021105ed
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-03-22 16:53:21 +0900

    :page_facing_up: modify testController
    web이 아니라 ResponseEntity를 반환하도록 설정

--- src/main/java/com/t2m/g2nee/shop/TestController.java
+++ src/main/java/com/t2m/g2nee/shop/TestController.java
@@ -1,19 +1,32 @@
 package com.t2m.g2nee.shop;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/shop")
 public class TestController {
 
+    HttpHeaders header = new HttpHeaders();
+
+
+    public TestController() {
+        header.setContentType(new MediaType("application","json"));
+    }
+
     @Value("${server.port}")
     private String port;
 
     @GetMapping("/hello")
-    public String getHello(){
-        return "hello g2nee shop : "+port;
+    public ResponseEntity<String> getHello(){
+        String result = "hello g2nee shop : "+port;
+        return new ResponseEntity<>(result,header,HttpStatus.OK);
     }
 }
