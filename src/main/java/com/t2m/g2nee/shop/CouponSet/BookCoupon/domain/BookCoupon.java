4ce26faff7edfa1982ecc53f8343e4fcc2c0c5cf
parkjh <jaeheep99@gmail.com>
2024-03-22 14:57:11 +0900

    :sparkles: Added CouponType, Bookcoupon

--- src/main/java/com/t2m/g2nee/shop/CouponSet/BookCoupon/domain/BookCoupon.java
+++ src/main/java/com/t2m/g2nee/shop/CouponSet/BookCoupon/domain/BookCoupon.java
@@ -1,5 +1,8 @@
 package com.t2m.g2nee.shop.CouponSet.BookCoupon.domain;
 
+import com.t2m.g2nee.shop.BookSet.Book.domain.Book;
+import com.t2m.g2nee.shop.CouponSet.CouponType.domain.CouponType;
+import com.t2m.g2nee.shop.MemberSet.Member.domain.Member;
 import lombok.*;
 
 import javax.persistence.*;
@@ -11,8 +14,12 @@ import javax.persistence.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class BookCoupon {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+@DiscriminatorValue("CouponType")
+public class BookCoupon extends CouponType{
 
+
+
+    @ManyToOne
+    @JoinColumn(name = "bookId")
+    private Book book;
 }
--- src/main/java/com/t2m/g2nee/shop/CouponSet/CouponType/domain/CouponType.java
+++ src/main/java/com/t2m/g2nee/shop/CouponSet/CouponType/domain/CouponType.java
@@ -1,4 +1,29 @@
 package com.t2m.g2nee.shop.CouponSet.CouponType.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "CouponTypes")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CouponType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long couponTypeId;
+    private String name;
+    private Integer period;
+    private Type type;
+    private Long discount;
+    private BigDecimal minimumOrderAmounnt;
+    private BigDecimal maximumDiscount;
+
+    public enum Type{
+        AMOUNT, PERCENT
+    }
 }
