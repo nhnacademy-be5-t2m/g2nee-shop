d144bde0e50be49b8a06d6c5a72cfef35f4f474b
origin <git@github.com:ehdals0405/PreProjectback.git>
2024-03-28 11:07:01 +0900

    :sparkles: setting querydsl

--- src/main/java/com/t2m/g2nee/shop/nhnstorage/ObjectService.java
+++ src/main/java/com/t2m/g2nee/shop/nhnstorage/ObjectService.java
@@ -2,10 +2,6 @@ package com.t2m.g2nee.shop.nhnstorage;
 
 import com.sun.istack.NotNull;
 import com.t2m.g2nee.shop.properties.NhnCloudStorageProperties;
-import com.t2m.g2nee.shop.properties.NhnCloudTokenProperties;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import lombok.Data;
 import lombok.NonNull;
@@ -13,7 +9,6 @@ import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpMessageConverterExtractor;
@@ -35,40 +30,6 @@ public class ObjectService {
         this.restTemplate = new RestTemplate(requestFactory);
         this.nhnCloudStorageProperties = nhnCloudStorageProperties;
         this.authService = authService;
-    }
-
-    public static void main(String[] args) {
-        final String storageUrl = "https://kr1-api-object-storage.nhncloudservice.com/v1";
-        final String auth = "/AUTH_fcb81f74e379456b8ca0e091d351a7af";
-        final String containerName = "/g2nee";
-        final String objectPath = "/thumbnail";
-        final String objectName = "TheMartian.jpeg";
-
-        final String authUrl = "https://api-identity-infrastructure.nhncloudservice.com/v2.0";
-        final String tenantId = "fcb81f74e379456b8ca0e091d351a7af";
-        final String username = "aokagami03@gmail.com";
-        final String password = "g2nee";
-
-        NhnCloudTokenProperties nhnCloudTokenProperties =
-                new NhnCloudTokenProperties(authUrl, tenantId, username, password);
-
-        AuthService authService = new AuthService(nhnCloudTokenProperties);
-        String tokenId = authService.requestToken();
-
-        NhnCloudStorageProperties properties = new NhnCloudStorageProperties(storageUrl, auth, containerName);
-        ObjectService objectService = new ObjectService(properties, authService);
-
-        try {
-            // 파일로 부터 InputStream 생성
-            File objFile = new File(objectPath + "/" + objectName);
-            InputStream inputStream = new FileInputStream(objFile);
-
-            // 업로드
-            objectService.uploadObject(tokenId, objectPath, objectName, inputStream);
-            System.out.println("\nUpload OK");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     /**
@@ -101,12 +62,10 @@ public class ObjectService {
         String url = this.getUrl(nhnCloudStorageProperties, objectPath, objectName);
 
         // InputStream을 요청 본문에 추가할 수 있도록 RequestCallback 오버라이드
-        final RequestCallback requestCallback = new RequestCallback() {
-            public void doWithRequest(final ClientHttpRequest request) throws IOException {
-                request.getHeaders().add("Content-Type", "multipart/form-data");
-                request.getHeaders().add("X-Auth-Token", tokenId);
-                IOUtils.copy(inputStream, request.getBody());
-            }
+        final RequestCallback requestCallback = request -> {
+            request.getHeaders().add("Content-Type", "multipart/form-data");
+            request.getHeaders().add("X-Auth-Token", tokenId);
+            IOUtils.copy(inputStream, request.getBody());
         };
 
         HttpMessageConverterExtractor<String> responseExtractor
