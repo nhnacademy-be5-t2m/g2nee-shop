95c5c117580e1dc987682de2389e6fb645f25aca
parkjh <jaeheep99@gmail.com>
2024-03-29 13:36:13 +0900

    :construction: ReviewImplService, ReviewImplRepository

--- src/main/java/com/t2m/g2nee/shop/review/controller/ReviewController.java
+++ src/main/java/com/t2m/g2nee/shop/review/controller/ReviewController.java
@@ -1,7 +1,15 @@
 package com.t2m.g2nee.shop.review.controller;
 
+import com.t2m.g2nee.shop.review.dto.response.ResponseGetProductReviewDto;
+import com.t2m.g2nee.shop.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +23,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping
 public class ReviewController {
+    private ReviewService reviewService;
     /**
      * 리뷰 리스트 조회
      *
      * @author : 박재희
      * @since : 1.0
      */
-    @GetMapping
-
+    @GetMapping//("/api/reviews/book/{bookId}")
+    public ResponseEntity/*pageresponse 필요*/ productReviewList(Pageable pageable, @PathVariable Long bookId){
+        Page<ResponseGetProductReviewDto> reviewDtos = reviewService.getProductReview(pageable, bookId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new reviewDtos);
+    }
 }
