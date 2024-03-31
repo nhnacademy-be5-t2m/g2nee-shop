2537bbbeb598002bfe6d98c419c03e7e2cf087e4
KimSu-Been <tnqls631@naver.com>
2024-04-01 00:00:12 +0900

    :recycle: add validation

--- src/main/java/com/t2m/g2nee/shop/bookset/category/dto/request/CategorySaveDto.java
+++ src/main/java/com/t2m/g2nee/shop/bookset/category/dto/request/CategorySaveDto.java
@@ -1,5 +1,7 @@
 package com.t2m.g2nee.shop.bookset.category.dto.request;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,9 +15,15 @@ import lombok.NoArgsConstructor;
 public class CategorySaveDto {
 
     //등록할 카테고리의 이름과 영문 이름
+    @Max(value = 50, message = "50자 미만으로 입력해 주세요.")
+    @NotBlank(message = "카테고리 명은 비울 수 없습니다.")
     private String categoryName;
+
+    @Max(value = 50, message = "50자 미만으로 입력해 주세요.")
+    @NotBlank(message = "카테고리 명은 비울 수 없습니다.")
     private String categoryEngName;
 
     //틍록할 카테고리의 바로 상위 카테고리Id
+    @NotBlank(message = "상위 카테고리가 없음!")
     private Long ancestorCategoryId;
 }
--- src/main/java/com/t2m/g2nee/shop/bookset/category/dto/request/CategoryUpdateDto.java
+++ src/main/java/com/t2m/g2nee/shop/bookset/category/dto/request/CategoryUpdateDto.java
@@ -1,6 +1,8 @@
 package com.t2m.g2nee.shop.bookset.category.dto.request;
 
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,12 +15,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CategoryUpdateDto {
 
+    @NotBlank(message = "카테고리 아이디가 없음!")
     private Long categoryId;
 
     //등록할 카테고리의 이름과 영문 이름
+    @Max(value = 50, message = "50자 미만으로 입력해 주세요.")
+    @NotBlank(message = "카테고리 명은 비울 수 없습니다.")
     private String categoryName;
+
+    @Max(value = 50, message = "50자 미만으로 입력해 주세요.")
+    @NotBlank(message = "카테고리 명은 비울 수 없습니다.")
     private String categoryEngName;
 
     //수정할 카테고리의 바로 상위 카테고리Id
+    @NotBlank(message = "상위 카테고리가 없음!")
     private Long ancestorCategoryId;
 }
