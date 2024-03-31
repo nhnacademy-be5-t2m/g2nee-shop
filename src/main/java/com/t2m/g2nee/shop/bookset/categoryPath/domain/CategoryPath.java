3f027032d52e0bfabb80a57d3e2ff5168f7f313d
KimSu-Been <tnqls631@naver.com>
2024-03-31 23:57:22 +0900

    :recycle: add equals and hash

--- src/main/java/com/t2m/g2nee/shop/bookset/categoryPath/domain/CategoryPath.java
+++ src/main/java/com/t2m/g2nee/shop/bookset/categoryPath/domain/CategoryPath.java
@@ -10,6 +10,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,6 +22,7 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class CategoryPath {
 
     @Id
