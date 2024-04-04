57404edd7c6ac9a764e8d787ae46d24756dbbf86
ehdals0405 <aokagami@naver.com>
2024-04-04 17:50:40 +0900

    :recycle: move method

--- src/main/java/com/t2m/g2nee/shop/bookset/book/repository/BookCustomRepositoryImpl.java
+++ src/main/java/com/t2m/g2nee/shop/bookset/book/repository/BookCustomRepositoryImpl.java
@@ -1,13 +1,17 @@
 package com.t2m.g2nee.shop.bookset.book.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.t2m.g2nee.shop.bookset.book.domain.Book;
 import com.t2m.g2nee.shop.bookset.book.domain.QBook;
+import com.t2m.g2nee.shop.bookset.book.dto.BookDto;
+import com.t2m.g2nee.shop.bookset.booktag.domain.QBookTag;
+import com.t2m.g2nee.shop.bookset.tag.domain.QTag;
+import java.util.List;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 public class BookCustomRepositoryImpl extends QuerydslRepositorySupport implements BookCustomRepository {
     public BookCustomRepositoryImpl() {
         super(Book.class);
     }
-
 
 }
--- src/main/java/com/t2m/g2nee/shop/bookset/bookcategory/repository/BookCategoryRepository.java
+++ src/main/java/com/t2m/g2nee/shop/bookset/bookcategory/repository/BookCategoryRepository.java
@@ -3,7 +3,12 @@ package com.t2m.g2nee.shop.bookset.bookcategory.repository;
 import com.t2m.g2nee.shop.bookset.bookcategory.domain.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
+
+    @Modifying
+    @Query("DELETE FROM BookCategory bc WHERE bc.book.bookId = :bookId")
+    void deleteByBookId(Long bookId);
 
 }
