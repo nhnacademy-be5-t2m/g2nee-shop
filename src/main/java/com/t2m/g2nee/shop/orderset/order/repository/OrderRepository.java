cc7cd788b637ecee71483696712a74d498f1be1c
KimSu-Been <tnqls631@naver.com>
2024-04-29 13:10:57 +0900

    :sparkles: 결제 승인, 취소, 조회

--- src/main/java/com/t2m/g2nee/shop/orderset/order/repository/OrderRepository.java
+++ src/main/java/com/t2m/g2nee/shop/orderset/order/repository/OrderRepository.java
@@ -1,6 +1,6 @@
-package com.t2m.g2nee.shop.orderset.Order.repository;
+package com.t2m.g2nee.shop.orderset.order.repository;
 
-import com.t2m.g2nee.shop.orderset.Order.domain.Order;
+import com.t2m.g2nee.shop.orderset.order.domain.Order;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
