8dd1ac69ca15605932c5c2e85963357bdd838b6c
KimSu-Been <tnqls631@naver.com>
2024-05-03 20:53:30 +0900

    :recycle: 충돌해결

---
+++ src/main/java/com/t2m/g2nee/shop/memberset/address/dto/response/AddressResponseDto.java
@@ -0,0 +1,17 @@
+package com.t2m.g2nee.shop.memberset.address.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressResponseDto {
+    private Long addressId;
+    private String address;
+    private String zipcode;
+    private String alias;
+    private String detail;
+    private Boolean isDefault;
+}
